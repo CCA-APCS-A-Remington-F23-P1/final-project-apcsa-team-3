76a228a19aa793ff2bcf12a894cc319376292bf0
m <86756928+mmaeki@users.noreply.github.com>
2023-12-06 09:11:03 -0800

    Update Set.java

--- Set.java
+++ Set.java
@@ -1,1 +1,27 @@
+import java.lang.Math;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Set extends Game {
+  private int score;
+  private int highScore;
+  
+  public Set() {
+    
+  }
+
+
+  public boolean isSet(Card a, Card b, Card c) {
+    //determines whether the three cards make a set
+  }
+
+  @override
+
+  public highScore(int score, int highScore) {
+    return (Math.max(score, highScore));
+  }
+
+
+
+  
+} 
