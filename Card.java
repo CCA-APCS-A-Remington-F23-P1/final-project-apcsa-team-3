28d411a6f10f8adc23742d6221f5be744a1fa477
krishmandadi <152903978+krishmandadi@users.noreply.github.com>
2023-12-06 09:01:42 -0800

    Update Card.java

--- Card.java
+++ Card.java
@@ -3,11 +3,13 @@ public class Card{
   private int number;
   private String shape;
   private String fill;
+  private String color;
 
-  public Card(int n, String s, String f){
+  public Card(int n, String s, String f, String c){
     number = n;
     shape = s;
     fill = f;
+    color = c;
   }
 
   public int getNumber(){
@@ -19,6 +21,9 @@ public class Card{
   public String getFill(){
     return fill;
   }
+  public String getColor(){
+    return color;
+  }
 
   public void setNumber(int n){
     number = n;
@@ -29,13 +34,16 @@ public class Card{
   public void setFill(String f){
     fill = f;
   }
+  public void setColor(String c){
+    color = c;
+  }
 
   public boolean equals(Card other){
-    return(number == other.getNumber() && shape.equals(other.getShape()) && fill.equals(other.getFill()));
+    return(number == other.getNumber() && shape.equals(other.getShape()) && fill.equals(other.getFill()) && color.equals(other.getColor()));
   }
 
   public String toString(){
-    return(number + " " + shape + "s filled with" + fill);
+    return(number + " " color + " " + shape + "s filled with" + fill);
   }
   
 }
