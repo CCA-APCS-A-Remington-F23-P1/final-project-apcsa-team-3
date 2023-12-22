e1599e504c4092e0dc5cb9e33f5f58f277bd60a0
m <86756928+mmaeki@users.noreply.github.com>
2023-12-21 23:27:30 -0800

    Update GravitySetBoard.java

--- GravitySetBoard.java
+++ GravitySetBoard.java
@@ -1,5 +1,39 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class GravitySetBoard extends SetBoard{
-  public GravitySetBoard(){
-    super();
+
+  Timer t;
+  TimerTask task;
+  private boolean timesUp;
+  private final long timeLimit = 20000;
+
+  private long startTime;
+  
+  public GravitySetBoard(){  
+    super(); 
+    t = new Timer();
+    task = new TimerTask(){
+      public void run(){
+        timesUp = true;
+      }
+    }
+  }
+  public void startTimer(){
+    startTime = System.currentTimeMillis();
+    t.schedule(task, timeLimit);
+  }
+
+  public long getTimeLeft(){
+    return System.currentTimeMillis() - startTime;
+  }
+
+  @Override
+  public boolean another(){
+    boolean n = false;
+    if(getTimeLeft() > timeLimit){
+      n = super.another();
+    }
+    return n;
   }
 }
